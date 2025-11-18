7cadeb89f58a9a2c6d71168487c04f86c9bbb9ef
Gustavo Marques <markz_gusta@hotmail.com>
2025-11-18 11:42:01 -0300

    Revert "Revert "feat(professor): CRUD completo""
    This reverts commit dcdd6e85e4677a4ad11d7def7178b40d5e9d1874.

--- src/view/inscritos/Cadastro.java
+++ src/view/inscritos/Cadastro.java
@@ -1,25 +1,26 @@
 package view.inscritos;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
 import java.awt.Font;
-import javax.swing.JTextField;
+
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import controller.inscricoes.inscricoesController;
 
 public class Cadastro extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField cpf;
+	private JTextField codDisc;
 
 	public Cadastro() {
 		setTitle("Cadastro");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 391, 358);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -31,24 +32,29 @@ public class Cadastro extends JFrame {
 		lblNewLabel.setBounds(30, 63, 119, 28);
 		contentPane.add(lblNewLabel);
 		
-		textField = new JTextField();
-		textField.setBounds(30, 90, 119, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		cpf = new JTextField();
+		cpf.setBounds(30, 90, 119, 20);
+		contentPane.add(cpf);
+		cpf.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(214, 90, 125, 20);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		codDisc = new JTextField();
+		codDisc.setBounds(214, 90, 125, 20);
+		contentPane.add(codDisc);
+		codDisc.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("Código de disciplina");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNewLabel_1.setBounds(214, 63, 125, 23);
 		contentPane.add(lblNewLabel_1);
 		
-		JButton btnNewButton = new JButton("Cadastrar");
-		btnNewButton.setBounds(142, 189, 89, 23);
-		contentPane.add(btnNewButton);
+		JButton btnCadastrar = new JButton("Cadastrar");
+		btnCadastrar.setBounds(142, 189, 89, 23);
+		contentPane.add(btnCadastrar);
+		
+		inscricoesController inscCtrl = new inscricoesController(cpf, codDisc);
+		
+		btnCadastrar.addActionListener(inscCtrl);
+		btnCadastrar.addActionListener(e -> dispose());
 
 	}
 
--- src/view/professor/CRUDProfessor.java
+++ src/view/professor/CRUDProfessor.java
@@ -1,54 +1,130 @@
 package view.professor;
-import view.professor.*;
-import java.awt.EventQueue;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JEditorPane;
 import javax.swing.JTextField;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.border.EmptyBorder;
+import javax.swing.text.MaskFormatter;
+
+import controller.professor.ProfessorController;
 
 public class CRUDProfessor extends JFrame {
 
 	private static final long serialVersionUID = 2L;
 	private JPanel contentPane;
+	private JTextField tfCPF;
+	private JTextField tfNome;
+	private JTextField tfArea;
+	private JTextField tfPontos;
+	
 	
 	public CRUDProfessor() {
 		setTitle("Professor");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		setLocation(200, 100);
+		setBounds(100, 100, 429, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLocationRelativeTo(null); 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		new Font("Segoe UI", Font.PLAIN, 14);
 		
-		JButton cadastrar = new JButton("Cadastrar");
-		JButton consultar = new JButton("Consultar");
-		JButton editar = new JButton("Editar");	
-		JButton deletar = new JButton("Deletar");
+		try {
+		    MaskFormatter mask = new MaskFormatter(" ###.###.###-##");
+		    mask.setPlaceholderCharacter('_');
+		    tfCPF = new JFormattedTextField(mask);
+		    tfCPF.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		    tfCPF.setBounds(27, 23, 157, 32);
+		    contentPane.add(tfCPF);
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
+		
+		tfNome = new JTextField();
+		tfNome.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		tfNome.setBounds(27, 76, 157, 32);
+		contentPane.add(tfNome);
+		tfNome.setColumns(10);
 
-        cadastrar.setBounds(29, 90, 120, 30);
-        consultar.setBounds(276, 90, 120, 30);
-        editar.setBounds(29, 173, 120, 30);
-        deletar.setBounds(276, 173, 120, 30);
-        
-        getContentPane().add(cadastrar);
-        getContentPane().add(consultar);
-        getContentPane().add(editar);
-		getContentPane().add(deletar);
-        
-        // Evento dos botões
 		
-		cadastrar.addActionListener(e -> new Cadastro().setVisible(true));
-		consultar.addActionListener(e -> new Consulta().setVisible(true));
-		editar.addActionListener(e -> new Edicao().setVisible(true));
-		deletar.addActionListener(e -> new Delete().setVisible(true));
+		tfArea = new JTextField();
+		tfArea.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		tfArea.setBounds(27, 131, 157, 32);
+		contentPane.add(tfArea);
+		tfArea.setColumns(10);
 		
+		tfPontos = new JTextField();
+		tfPontos.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		tfPontos.setBounds(215, 131, 157, 32);
+		contentPane.add(tfPontos);
+		tfPontos.setColumns(10);
 		
+		JButton btnConsultar = new JButton("Consultar");
+		btnConsultar.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		btnConsultar.setBounds(215, 23, 157, 32);
+		contentPane.add(btnConsultar);
+		
+		JButton btnCadastrar = new JButton("Cadastrar");
+		btnCadastrar.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		btnCadastrar.setBounds(27, 187, 157, 32);
+		contentPane.add(btnCadastrar);
+		
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		btnLimpar.setBounds(215, 187, 157, 32);
+		contentPane.add(btnLimpar);
+		
+		addPlaceholder(tfNome, " Nome");
+		addPlaceholder(tfArea, " Área de conhecimento");
+		addPlaceholder(tfPontos, " Quantidade de Pontos");
+		
+		ProfessorController profCtrl = new ProfessorController(tfNome, tfCPF, tfArea, tfPontos);
+		
+		btnCadastrar.addActionListener(profCtrl);
+		btnLimpar.addActionListener(e -> limparCampos());
+		btnConsultar.addActionListener(profCtrl);
+		
+	}
+	
+	
+	public void limparCampos() {
+		tfCPF.setText("");
+		tfCPF.setCaretPosition(0);
+		tfNome.setText("");
+		tfArea.setText("");
+		tfPontos.setText("");
+		
+		addPlaceholder(tfNome, " Nome");
+		addPlaceholder(tfArea, " Área de conhecimento");
+		addPlaceholder(tfPontos, " Quantidade de Pontos");
+	}
+	
+	private static void addPlaceholder(JTextField field, String placeholder) {
+	    field.setForeground(Color.GRAY);
+	    field.setText(placeholder);
 
+	    field.addFocusListener(new FocusAdapter() {
+	        @Override
+	        public void focusGained(FocusEvent e) {
+	            if (field.getText().equals(placeholder)) {
+	                field.setText("");
+	                field.setForeground(Color.BLACK);
+	            }
+	        }
+
+	        @Override
+	        public void focusLost(FocusEvent e) {
+	            if (field.getText().isEmpty()) {
+	                field.setForeground(Color.GRAY);
+	                field.setText(placeholder);
+	            }
+	        }
+	    });
 	}
 }
